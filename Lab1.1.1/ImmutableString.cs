a9a1ee538df50fd32c2841ba53250232c41d14d6
Mikhail <gromov1505@gmail.com>
2022-03-30 19:50:53 +0300

    Добавьте файлы проекта.

---
+++ Lab1.1.1/ImmutableString.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1._1._1
+{
+    class ImmutableString
+    {
+        private readonly string immutableString;
+        public static ImmutableString Create(string sourse) => new ImmutableString(sourse);
+        private ImmutableString(string source) => immutableString = source;
+
+        public static ImmutableString operator +(ImmutableString str1, ImmutableString str2)
+            => new ImmutableString(str1.immutableString + str2.immutableString);
+        public static ImmutableString operator +(string str1, ImmutableString str2)
+            => new ImmutableString(str1 + str2.immutableString);
+
+        #region Methods
+        public ImmutableString Substring(int startIndex, int length)
+            => new ImmutableString(immutableString.Substring(startIndex, length));
+        public ImmutableString Remove(int startIndex, int count)
+            => new ImmutableString(immutableString.Remove(startIndex, count));
+        public override string ToString() => immutableString;
+        public ImmutableString Replace(string oldValue, string newValue)
+            => new ImmutableString(immutableString.Replace(oldValue, newValue));
+        #endregion
+
+        #region Properties
+        public char this[int index] => immutableString[index];
+        public int Length => immutableString.Length;
+
+        #endregion    
+    }
+    
+}
---
+++ Lab1.1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Создайте свой иммутабельный (immutable) тип.При изменении какого-либо из полей
+//создается новый объект. В качестве примера можете взять тип String.
+
+using System;
+
+namespace Lab1._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var imsSource = ImmutableString.Create("Why the hell am I a double immutable string");
+            
+            var imsMutable = imsSource.Remove(7, 2);
+            Console.WriteLine(imsMutable);
+
+            var imsDouble = imsSource.Substring(0, 6);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
